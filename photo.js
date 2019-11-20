a05057a897ecdd5b930a5108c707de38154ede06
KkakaMann <172830791@qq.com>
2019-11-20 15:15:23 +0800

    photo.js v2.2.9

--- photo.js
+++ photo.js
@@ -1,5 +1,5 @@
 /**
- * photo.js v2.2.8
+ * photo.js v2.2.9
  * @author KkakaMann
  * @mailto KkakaMann24@gmail.com  KkakaMann@163.com
  */
@@ -122,7 +122,6 @@ $(function (){
                 xlimgindex=xlphotolength-1;
             rerotateImg();
             processImg();
-            // originalimg();
         };
         var rotateImg=function (rotate){
             xlrotater+=rotate;
@@ -360,7 +359,13 @@ $(function (){
             directionLast();
             rerotateImg();
             return !processImg();
-            // originalimg();
+        }).on('mousewheel DOMMouseScroll', function (e) {
+            if($.browser.msie&&$.browser.version === '6.0')
+                return false;
+            if(!isdrag){
+                var delta=(e.originalEvent.wheelDelta && (e.originalEvent.wheelDelta > 0 ? 1 : -1)) || (e.originalEvent.detail && (e.originalEvent.detail > 0 ? -1 : 1));
+                delta > 0 ? $xlphotospageListprve.click() : $xlphotospageListprve.next().next().click();
+            }
         });
         dom.on('keyup',function (event){
             var code=event.keyCode || event.which || event.charCode;
